9eecf2b3c42e1d41b4abf2954625cc7a9028cbd4
Evan Walters <evanatyourservice@gmail.com>
2025-08-29 19:01:06 -0400

    quad WIP

--- distributed_kron/__init__.py
+++ distributed_kron/__init__.py
@@ -1,1 +1,12 @@
 from distributed_kron.kron import kron, scale_by_kron, get_opt_state_partition_specs, precond_update_prob_schedule
+from distributed_kron.quad import quad, scale_by_quad, get_opt_state_partition_specs_quad
+
+__all__ = [
+    "kron",
+    "scale_by_kron",
+    "get_opt_state_partition_specs",
+    "precond_update_prob_schedule",
+    "quad",
+    "scale_by_quad",
+    "get_opt_state_partition_specs_quad",
+]
